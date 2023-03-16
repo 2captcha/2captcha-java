3f9ecab926631ba00084c09d5056f334694b2c8c
zatvorius <zatvorius@gmail.com>
2023-03-16 13:40:33 +0400

    Add files via upload

---
+++ src/main/java/examples/GeeTestV4Example.java
@@ -0,0 +1,26 @@
+package examples;
+
+import com.twocaptcha.TwoCaptcha;
+import com.twocaptcha.captcha.GeeTestV4;
+
+public class GeeTestV4Example {
+
+    public static void main(String[] args) {
+        TwoCaptcha solver = new TwoCaptcha("YOUR_API_KEY");
+
+        GeeTestV4 captcha = new GeeTestV4();
+        captcha.setCaptchaId("72bf15796d0b69c43867452fea615052");
+        captcha.setApiServer("api-na.geetest.com");
+        captcha.setChallenge("12345678abc90123d45678ef90123a456b");
+        captcha.setUrl("https://mysite.com/captcha.html");
+
+
+        try {
+            solver.solve(captcha);
+            System.out.println("Captcha solved: " + captcha.getCode());
+        } catch (Exception e) {
+            System.out.println("Error occurred: " + e.getMessage());
+        }
+    }
+
+}
