477ea8fd4a4eef2e5d79495eed16d3399778d7e1
zatvorius <zatvorius@gmail.com>
2023-03-16 15:51:53 +0400

    Add files via upload

---
+++ src/main/java/examples/LeminOptionsExample.java
@@ -0,0 +1,29 @@
+package examples;
+
+import com.twocaptcha.TwoCaptcha;
+import com.twocaptcha.captcha.Lemin;
+
+public class LeminOptionsExample {
+
+    public static void main(String[] args) {
+        TwoCaptcha solver = new TwoCaptcha("YOUR_API_KEY");
+        solver.setSoftId(0);
+        solver.setDefaultTimeout(120);
+        solver.setRecaptchaTimeout(600);
+        solver.setPollingInterval(10);
+
+        Lemin captcha = new Lemin();      
+        captcha.setСaptchaId("CROPPED_d3d4d56_73ca4008925b4f83a8bed59c2dd0df6d");
+        captcha.setUrl("http://sat2.aksigorta.com.tr");
+        captcha.setApiServer("api.leminnow.com");
+        captcha.setProxy("HTTPS", "login:password@IP_address:PORT");
+        
+        try {
+            solver.solve(captcha);
+            System.out.println("Captcha solved: " + captcha.getCode());
+        } catch (Exception e) {
+            System.out.println("Error occurred: " + e.getMessage());
+        }
+    }
+
+}
