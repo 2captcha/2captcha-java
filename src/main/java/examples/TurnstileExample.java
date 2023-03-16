809309d6261fb334c4adaa0616437bc013c3b7b9
zatvorius <zatvorius@gmail.com>
2023-03-16 15:57:22 +0400

    Add files via upload

---
+++ src/main/java/examples/TurnstileExample.java
@@ -0,0 +1,23 @@
+package examples;
+
+import com.twocaptcha.TwoCaptcha;
+import com.twocaptcha.captcha.Turnstile;
+
+public class TurnstileExample {
+
+    public static void main(String[] args) {
+        TwoCaptcha solver = new TwoCaptcha("YOUR_API_KEY");
+
+        Turnstile captcha = new Turnstile();
+        captcha.setSiteKey("0x4AAAAAAAChNiVJM_WtShFf");
+        captcha.setUrl("https://ace.fusionist.io");
+
+        try {
+            solver.solve(captcha);
+            System.out.println("Captcha solved: " + captcha.getCode());
+        } catch (Exception e) {
+            System.out.println("Error occurred: " + e.getMessage());
+        }
+    }
+
+}
---
+++ src/main/java/examples/TurnstileOptionsExample.java
@@ -0,0 +1,29 @@
+package examples;
+
+import com.twocaptcha.TwoCaptcha;
+import com.twocaptcha.captcha.Turnstile;
+
+public class TurnstileOptionsExample {
+
+    public static void main(String[] args) {
+        TwoCaptcha solver = new TwoCaptcha("YOUR_API_KEY");
+        solver.setHost("rucaptcha.com");
+        solver.setSoftId(0);
+        solver.setDefaultTimeout(120);
+        solver.setRecaptchaTimeout(600);
+        solver.setPollingInterval(10);
+        
+        Turnstile captcha = new Turnstile();
+        captcha.setSiteKey("0x4AAAAAAAChNiVJM_WtShFf");
+        captcha.setUrl("https://ace.fusionist.io");
+        captcha.setProxy("HTTPS", "login:password@IP_address:PORT");
+
+        try {
+            solver.solve(captcha);
+            System.out.println("Captcha solved: " + captcha.getCode());
+        } catch (Exception e) {
+            System.out.println("Error occurred: " + e.getMessage());
+        }
+    }
+
+}
