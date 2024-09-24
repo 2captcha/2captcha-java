87b7ead7f66abdd974d481059102f6908c188ee5
dzmitry-duboyski <dzmitry.duboyski@gmail.com>
2024-09-24 13:48:41 +0400

    Update MtCaptcha examples

--- src/main/java/examples/MtCaptchaOptionsExample.java
+++ src/main/java/examples/MtCaptchaOptionsExample.java
@@ -3,14 +3,19 @@ package examples;
 import com.twocaptcha.TwoCaptcha;
 import com.twocaptcha.captcha.MtCaptcha;
 
-public class MtCaptchaExample {
-
+public class MtCaptchaOptionsExample {
     public static void main(String[] args) {
         TwoCaptcha solver = new TwoCaptcha(args[0]);
+        solver.setHost("rucaptcha.com");
+        solver.setSoftId(0);
+        solver.setDefaultTimeout(120);
+        solver.setRecaptchaTimeout(600);
+        solver.setPollingInterval(10);
 
         MtCaptcha mtCaptcha = new MtCaptcha();
         mtCaptcha.setSiteKey("MTPublic-KzqLY1cKH");
         mtCaptcha.setPageUrl("https://2captcha.com/demo/mtcaptcha");
+        mtCaptcha.setProxy("HTTPS", "login:password@IP_address:PORT");
 
         try {
             solver.solve(mtCaptcha);
