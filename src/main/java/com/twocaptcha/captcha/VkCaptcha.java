94c5a024dcde79a58ed40fb7bfeb4b7bb1245dc7
victorkowalski <vkowalski@yandex.ru>
2025-10-14 11:37:49 +0300

    VkCaptcha

--- src/main/java/com/twocaptcha/captcha/VkCaptcha.java
+++ src/main/java/com/twocaptcha/captcha/VkCaptcha.java
@@ -4,6 +4,14 @@ public class VkCaptcha extends Captcha {
 
     public VkCaptcha() {
         super();
-        params.put("method", "VKCaptchaImageTask");
+        params.put("method", "vkimage");
+    }
+
+    public void setSteps(String steps) {
+        params.put("steps", steps);
+    }
+
+    public void setImageBase64(String imageBase64) {
+        params.put("body", imageBase64);
     }
 }
