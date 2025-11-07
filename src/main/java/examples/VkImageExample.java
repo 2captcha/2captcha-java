2fd9214c8cadd1d6a0d8f4da6b6a0e9bbc1c41b2
victorkowalski <vkowalski@yandex.ru>
2025-11-07 08:22:43 +0300

    vkcaptcha file added

--- src/main/java/examples/VkImageExample.java
+++ src/main/java/examples/VkImageExample.java
@@ -1,8 +1,6 @@
 package examples;
 
 import com.twocaptcha.TwoCaptcha;
-import com.twocaptcha.captcha.Canvas;
-import com.twocaptcha.captcha.Rotate;
 import com.twocaptcha.captcha.VkCaptcha;
 
 import java.io.IOException;
@@ -10,7 +8,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Base64;
 
-public class VkExample {
+public class VkImageExample {
 
     private void imageBased(String apiKey) throws IOException {
         TwoCaptcha solver = new TwoCaptcha(apiKey);
@@ -31,25 +29,9 @@ public class VkExample {
         }
     }
 
-    private void tokenBased(String apiKey) throws IOException {
-        TwoCaptcha solver = new TwoCaptcha(apiKey);
-        VkCaptcha captcha = new VkCaptcha("vkcaptcha");
-        captcha.setRedirectUri("https://id.vk.com/not_robot_captcha?domain=vk.com&session_token=eyJ....HGsc5B4LyvjA&variant=popup&blank=1");
-        captcha.setuserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36");
-        captcha.setProxy("http", "1.2.3.4");
-
-        try {
-            solver.solve(captcha);
-            System.out.println("Captcha solved: " + captcha.getCode());
-        } catch (Exception e) {
-            System.out.println("Error occurred: " + e.getMessage());
-        }
-    }
-
     public static void main(String[] args) throws Exception {
-        VkExample vkExample = new VkExample();
+        VkImageExample vkExample = new VkImageExample();
         vkExample.imageBased(args[0]);
-        //vkExample.tokenBased(args[0]);
     }
 
 }
