f4e1c90413ef31d392d7353384cea53b0898a95d
victorkowalski <vkowalski@yandex.ru>
2026-04-08 19:11:29 +0300

    ExampleBase64

--- src/main/java/com/twocaptcha/v2/ExampleBase64.java
+++ src/main/java/com/twocaptcha/v2/ExampleBase64.java
@@ -2,19 +2,30 @@ package com.twocaptcha.v2;
 
 import org.json.JSONObject;
 
-public class Example {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
+
+public class ExampleBase64 {
 
     private String key;
 
-    public Example(String key) {
+    public ExampleBase64(String key) {
         this.key = key;
     }
 
-    private void resolve(){
+    private void resolve() throws IOException {
         ApiClient apiClient = new ApiClient(this.key);
+
+        byte[] bytes = Files.readAllBytes(Paths.get("src/main/resources/rotate.jpg"));
+        String base64EncodedImage = Base64.getEncoder().encodeToString(bytes);
+
         JSONObject innerJsonObject = new JSONObject()
-                .put("type", "TextCaptchaTask")
-                .put("comment", "If tomorrow is Saturday, what day is today?");
+                .put("type", "RotateTask")
+                .put("body", base64EncodedImage)
+                .put("comment", "position the image properly")
+                .put("angle", 60);
 
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("clientKey", this.key);
@@ -30,8 +41,8 @@ public class Example {
 
     }
 
-    public static void main(String[] args) {
-        Example example = new Example(args[0]);
+    public static void main(String[] args) throws IOException {
+        ExampleBase64 example = new ExampleBase64(args[0]);
         example.resolve();
     }
 
