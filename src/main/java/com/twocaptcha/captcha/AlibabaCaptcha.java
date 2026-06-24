340dfc619a18199a1f542fd8558c10fa9100cdf5
victorkowalski <vkowalski@yandex.ru>
2026-06-24 07:28:46 +0300

    params added

--- src/main/java/com/twocaptcha/captcha/AlibabaCaptcha.java
+++ src/main/java/com/twocaptcha/captcha/AlibabaCaptcha.java
@@ -15,4 +15,15 @@ public class AlibabaCaptcha extends Captcha {
         params.put("prefix", prefix);
     }
 
+    public void setUserId(String userId) { params.put("user_id", userId); }
+
+    public void setUserUserId(String userUserId) { params.put("user_user_id", userUserId); }
+
+    public void setVerifyType(String verifyType) { params.put("verify_type", verifyType); }
+
+    public void setRegion(String region) { params.put("region", region); }
+
+    public void setUserCertifyId(String userCertifyId) { params.put("user_certify_id", userCertifyId); }
+
+    public void setApiGetLib(String apiGetLib) { params.put("api_get_lib", apiGetLib); }
 }
